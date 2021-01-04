75fddbf7dbd85bbb76a4a66e8ecfaad6cc3ae7ea
David Tooley <detooley@gmail.com>
2021-01-04 12:03:35 -0500

    Fixed case consistent with PEP8.

--- tsdr_file.py
+++ tsdr_file.py
@@ -8,21 +8,21 @@ import json
 class TsdrFile():
     # Alllows users to set serial number when instantiating
     # Includes a demo serial number for lazy people
-    def __init__(self, serNum=['88855299']):
-        self.setFile(serNum)
+    def __init__(self, sernum=['88855299']):
+        self.setfile(sernum)
 
     # Allows user to change serial number
     # Do a try-catch and retry wheb not 200
-    def setFile(self, serNum):
-        for x in serNum:
-            url = 'https://tsdrsec.uspto.gov/ts/cd/casestatus/sn' + serNum + \
+    def setfile(self, sernum):
+        for x in sernum:
+            url = 'https://tsdrsec.uspto.gov/ts/cd/casestatus/sn' + sernum + \
             '/info.json'
             response = requests.get(url)
             text = response.text
             dikt = json.loads(text)
             # Set private variables used by other methodsB
-            self.__jsonFile = text
-            self.__dictFile = dikt['trademarks'][0]
+            self.__jsonfile = text
+            self.__dictfile = dikt['trademarks'][0]
 
     # Methods for accessing data
     # Returns the descriptions of goods and services and their class numbers
@@ -30,56 +30,56 @@ class TsdrFile():
     @property
     def ids(self):
         gsIds = {}
-        for x in self.__dictFile['gsList']:
-            gsIds.update({x['primeClassCode']: x['description']})
-        return(gsIds)
+        for x in self.__dictfile['gslist']:
+            gsIds.update({x['primeclasscode']: x['description']})
+        return(gsids)
 
     # Returns serial number
     @property
     def serNum(self):
-        return(str(self.__dictFile['status']['serialNumber']))
+        return(str(self.__dictfile['status']['serialNumber']))
 
     # Returns mark literal
     @property
     def mark(self):
-        return(self.__dictFile['status']['markElement'])
+        return(self.__dictfile['status']['markElement'])
 
     # Returns the registration status
     @property
     def status(self):
-        return(str(self.__dictFile['status']['status']))
+        return(str(self.__dictfile['status']['status']))
 
 class FileStatus():
-    def __init__(self, serNum, cl, desc):
+    def __init__(self, sernum, cl, desc):
         self.__file = TsdrFile(serNum)
         self.__cl = cl
         self.__desc = desc
 
-    def getRegStatus(self):
+    def getregstatus(self):
         return(self.__file.status)
 
-    def getIdStatus(self):
+    def getidstatus(self):
         print(self.__file.ids)
 
 class ListHandler():
     def __init__(self, fileLoc):
         pass
 
-    def parseFile():
+    def parsefile():
         self.__file = {}
         for x in serNum:
-            self.__file[x] = tsdrFile(x)
+            self.__file[x] = TsdrFile(x)
 
 class IdComparison():
     def __init__(fileId, compTerm, cl):
-        self.__fileId = fileId
-        self.__compTerm = compTerm
+        self.__fileid = fileid
+        self.__compterm = compterm
         self._cl = cl
 
-    def __formatId():
+    def __formatid():
         pass
 
-    def compareId():
+    def compareid():
         pass
 
 
@@ -88,5 +88,5 @@ y = '009'
 z = 'shirts'
 
 t = FileStatus(x, y, z)
-t.getRegStatus()
-t.getIdStatus()
+t.getregstatus()
+t.getidstatus()
